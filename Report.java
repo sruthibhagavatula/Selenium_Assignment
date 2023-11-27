f5b04bb4d957e871e75f3deb22ad5d31c2cc5e33
bhagavatula sruthi <sruthir91@gmail.com>
2023-11-27 21:28:45 +0530

    Week2Day1Assignment

---
+++ Report.java
@@ -0,0 +1,13 @@
+package week2AssignmentDay1;
+
+public class Report {
+	public static void main(String[] args) {
+		Student student = new Student();
+		System.out.println(student.studentName);
+		System.out.println(student.rollNumber);
+		System.out.println(student.collegeName);
+		System.out.println(student.markScored);
+		System.out.println(student.cgpa);
+	}
+
+}
